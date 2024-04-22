bc2160bdf4f2d8c898a7a8687abf55fe383bacd0
Павел <128207591+Tvenx@users.noreply.github.com>
2024-04-22 20:28:34 +0300

    Update RotationY.cs

--- GameinElbow/Assets/script/RotationY.cs
+++ GameinElbow/Assets/script/RotationY.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RotationY : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 90f;
-    void Update()
+    private void Update()
     {
         float rotationAmount = rotationSpeed * Time.deltaTime;
         transform.Rotate(Vector3.up, rotationAmount);
