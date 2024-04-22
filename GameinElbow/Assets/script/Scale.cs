45bbb368b796f474c69421a05f111530d33f80d5
Павел <128207591+Tvenx@users.noreply.github.com>
2024-04-22 20:29:09 +0300

    Update Scale.cs

--- GameinElbow/Assets/script/Scale.cs
+++ GameinElbow/Assets/script/Scale.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Scale : MonoBehaviour
 {
     [SerializeField] private float growthRate = 0.1f;
-    void Update()
+    
+    private void Update()
     {
         Vector3 scaleChange = Vector3.one * growthRate * Time.deltaTime;
         transform.localScale += scaleChange;
